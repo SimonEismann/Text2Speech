4ff22051b66f35d6baa2a9004e63c3881aba1f79
Simon <simon.eismann@uni-wuerzburg.de>
2018-06-21 20:04:39 +0200

    added external call to FileServer

--- src/main/java/servlets/HelloServlet.java
+++ src/main/java/servlets/HelloServlet.java
@@ -5,6 +5,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -14,17 +17,19 @@ public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 3725150619150580957L;
 
 	@Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
 
-        writer.println("<html>");
-        writer.println("<head><title>Java Beispiel Servlet</title></head>");
-        writer.println("<body>");
-        writer.println("<h1>Servlet Beispiel</h1>");
-        writer.println("<p>Inhalt der HTML-Seite</p>");
-        writer.println("<body>");
-        writer.println("</html>");
+		String itSays = ClientBuilder.newClient().target("http://" + System.getenv("FileServerIp")).path("FileServer/hello").request(MediaType.TEXT_PLAIN).get(String.class);
 
-        writer.close();
-    }
+		writer.println("<html>");
+		writer.println("<head><title>Java Beispiel Servlet</title></head>");
+		writer.println("<body>");
+		writer.println("<h1>Servlet Beispiel</h1>");
+		writer.println("<p>The file server says: " + itSays + "</p>");
+		writer.println("<body>");
+		writer.println("</html>");
+
+		writer.close();
+	}
 }
