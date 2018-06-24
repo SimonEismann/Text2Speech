cb3151ee1c7bab9c9a986d754c489a0428dff1fc
Simon <simon.eismann@uni-wuerzburg.de>
2018-06-24 16:54:06 +0200

    added text2speech

--- src/main/java/servlets/Text2SpeechServlet.java
+++ src/main/java/servlets/Text2SpeechServlet.java
@@ -5,6 +5,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sound.sampled.AudioInputStream;
+
+import marytts.LocalMaryInterface;
+import marytts.exceptions.MaryConfigurationException;
+import marytts.exceptions.SynthesisException;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -17,6 +22,7 @@ public class Text2SpeechServlet extends HttpServlet {
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String content = req.getParameter("text");
+		// init mary
 		PrintWriter writer = resp.getWriter();
 //		WebTarget target = ClientBuilder.newClient().target("http://" + System.getenv("FileServerIp")).path("FileServer/rest/hello");
 //		String itSays = target.request(MediaType.TEXT_PLAIN).get(String.class);
@@ -29,5 +35,23 @@ public class Text2SpeechServlet extends HttpServlet {
 		writer.println("</html>");
 
 		writer.close();
+	}
+	
+	public AudioInputStream text2speech(String text) {
+		LocalMaryInterface mary = null;
+		try {
+			mary = new LocalMaryInterface();
+		} catch (MaryConfigurationException e) {
+			throw new IllegalStateException("Could not initialize MaryTTS interface: " + e.getMessage());
+		}
+
+		// synthesize
+		AudioInputStream audio = null;
+		try {
+			audio = mary.generateAudio(text);
+		} catch (SynthesisException e) {
+			throw new IllegalStateException("Synthesis failed: " + e.getMessage());
+		}
+		return audio;
 	}
 }
