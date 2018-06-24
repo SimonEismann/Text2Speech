6eb197f53244782da456c9d10ca095f212f1244a
Simon <simon.eismann@uni-wuerzburg.de>
2018-06-24 18:33:40 +0200

    debug framesize

--- src/main/java/servlets/Text2SpeechServlet.java
+++ src/main/java/servlets/Text2SpeechServlet.java
@@ -48,7 +48,7 @@ public class Text2SpeechServlet extends HttpServlet {
 		}
 		
 		PrintWriter writer = resp.getWriter();
-		writer.write(audioS.getFormat().toString());
+		writer.write(audioS.getFormat().getFrameSize());
 		writer.close();
 	}
 	
