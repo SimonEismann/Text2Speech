c14fa0fb11379fbcc8b58fcd445e87a4530cee13
Simon <simon.eismann@uni-wuerzburg.de>
2018-06-24 16:18:56 +0200

    added first draft of upload form

--- src/main/java/servlets/IndexServlet.java
+++ src/main/java/servlets/IndexServlet.java
@@ -5,32 +5,29 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/*")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 3725150619150580957L;
 
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter writer = resp.getWriter();
-
-		System.out.println();
-		
-		WebTarget target = ClientBuilder.newClient().target("http://" + System.getenv("FileServerIp")).path("FileServer/rest/hello");
-		String itSays = target.request(MediaType.TEXT_PLAIN).get(String.class);
+//		WebTarget target = ClientBuilder.newClient().target("http://" + System.getenv("FileServerIp")).path("FileServer/rest/hello");
+//		String itSays = target.request(MediaType.TEXT_PLAIN).get(String.class);
 		 
 		writer.println("<html>");
-		writer.println("<head><title>Java Beispiel Servlet</title></head>");
+		writer.println("<head><title>Text2Speech Upload</title></head>");
 		writer.println("<body>");
-		writer.println("<h1>Servlet Beispiel</h1>");
-		writer.println("<p>The file server says: " + itSays + "</p>");
+		writer.println("<h1>Text2Speech Upload</h1>");
+		writer.println("<form action=\"upload\" method=\"post\">");
+		writer.println("<textarea rows=\"4\" cols=\"50\">Hello World!</textarea>");
+		writer.println("<input type=\"submit\" value=\"Submit\">x");
+		writer.println("</form>");
 		writer.println("<body>");
 		writer.println("</html>");
 
