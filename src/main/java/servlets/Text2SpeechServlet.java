9ab95bff036adf267db28216acbfcc2d8dd5c951
Simon <simon.eismann@uni-wuerzburg.de>
2018-06-24 17:02:43 +0200

    text2speech servlet now returns audio

--- src/main/java/servlets/Text2SpeechServlet.java
+++ src/main/java/servlets/Text2SpeechServlet.java
@@ -1,18 +1,21 @@
 package servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sound.sampled.AudioInputStream;
 
+import org.apache.commons.io.IOUtils;
+
 import marytts.LocalMaryInterface;
 import marytts.exceptions.MaryConfigurationException;
 import marytts.exceptions.SynthesisException;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStream;
 
 @WebServlet("/text2speech")
 public class Text2SpeechServlet extends HttpServlet {
@@ -22,19 +25,14 @@ public class Text2SpeechServlet extends HttpServlet {
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String content = req.getParameter("text");
-		// init mary
-		PrintWriter writer = resp.getWriter();
+		AudioInputStream audio = text2speech(content);
+		
+		resp.setContentType("audio/x-wav");
+		resp.setHeader("Accept-Ranges", "bytes");
+		IOUtils.copy(audio, resp.getOutputStream());
+		
 //		WebTarget target = ClientBuilder.newClient().target("http://" + System.getenv("FileServerIp")).path("FileServer/rest/hello");
 //		String itSays = target.request(MediaType.TEXT_PLAIN).get(String.class);
-		 
-		writer.println("<html>");
-		writer.println("<head><title>Text2Speech Upload</title></head>");
-		writer.println("<body>");
-		writer.println("<p>" + content + "</p>");
-		writer.println("<body>");
-		writer.println("</html>");
-
-		writer.close();
 	}
 	
 	public AudioInputStream text2speech(String text) {
