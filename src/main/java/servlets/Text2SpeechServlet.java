0d31f507f9ccf916763ca28e3749c758b08552fb
Simon <simon.eismann@uni-wuerzburg.de>
2018-06-24 17:30:37 +0200

    now sends double array to fileServer

--- src/main/java/servlets/Text2SpeechServlet.java
+++ src/main/java/servlets/Text2SpeechServlet.java
@@ -6,14 +6,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sound.sampled.AudioInputStream;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 
 import org.apache.commons.io.IOUtils;
 
 import marytts.LocalMaryInterface;
 import marytts.exceptions.MaryConfigurationException;
 import marytts.exceptions.SynthesisException;
+import marytts.util.MaryUtils;
+import marytts.util.data.audio.MaryAudioUtils;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/text2speech")
 public class Text2SpeechServlet extends HttpServlet {
@@ -23,16 +30,17 @@ public class Text2SpeechServlet extends HttpServlet {
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String content = req.getParameter("text");
-		AudioInputStream audio = text2speech(content);
+		double[] audio = text2speech(content);
 		
-		resp.setContentType("audio/x-wav");
-		resp.setHeader("Accept-Ranges", "bytes");
-		IOUtils.copy(audio, resp.getOutputStream());
+		WebTarget target = ClientBuilder.newClient().target("http://" + System.getenv("FileServerIp")).path("FileServer/rest/save");
+		String itSays = target.request(MediaType.TEXT_PLAIN).post(Entity.entity(audio, MediaType.APPLICATION_JSON), String.class);
 		
-//		WebTarget target = ClientBuilder.newClient().target("http://" + System.getenv("FileServerIp")).path("FileServer/rest/hello");
-//		String itSays = target.request(MediaType.TEXT_PLAIN).get(String.class);
+		PrintWriter writer = resp.getWriter();
+		writer.write(itSays);
+		writer.close();
 	}
-	public AudioInputStream text2speech(String text) {
+	
+	public double[] text2speech(String text) {
 		LocalMaryInterface mary = null;
 		try {
 			mary = new LocalMaryInterface();
@@ -48,6 +56,6 @@ public class Text2SpeechServlet extends HttpServlet {
 			throw new IllegalStateException("Synthesis failed: " + e.getMessage());
 		}
 		
-		return audio;
+		return MaryAudioUtils.getSamplesAsDoubleArray(audio);
 	}
 }
